99eeac7d0c1f30a67f03ca5f2cf44523c302412a
niall moore <moore.nialls@gmail.com>
2019-11-27 16:54:27 +0000

    creating website layout

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <html>
     <head>
         <title> On Line Store </title>
+        <link rel="stylesheet" href="styles/style.css" media="all">
     </head>
 
         <body>
-        
-        
-        
-        
-        
-        </body>
-
-
-
-
+<!--main container-->
+	            <div class="main-wrapper">  
+                        <img id="logo" src="images/image-11.jpeg"/>
+                        <img id="banner" src="images/image-22.jpeg"/>
+                </div>
+                    <div class="menubar">
+                    </div>
+                
+                <div class="content-wraper">
+                <div id="sidebar"></div>
+                <div id="content_area"></div>
+	     </body>
 </html>
